8913fcc32e1012d9a4b81381c90cd8eb6abc4aff
apsampaio <andre03.sampaio@gmail.com>
2021-06-22 21:15:09 -0300

    add tag controller and route

--- valoriza/src/routes.ts
+++ valoriza/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "./controller/CreateUserController";
+import { CreateTagController } from "./controller/CreateTagController";
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 const router = Router();
 
 router.post("/users", createUserController.handle);
+router.post("/tags", createTagController.handle);
 
 export { router };
