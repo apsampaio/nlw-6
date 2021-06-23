2661d5ad420d77373eef03214cff9d5fefbe65a5
apsampaio <andre03.sampaio@gmail.com>
2021-06-22 21:36:16 -0300

    add middleware

--- valoriza/src/routes.ts
+++ valoriza/src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { CreateUserController } from "./controller/CreateUserController";
 import { CreateTagController } from "./controller/CreateTagController";
 
+import ensureAdmin from "./middlewares/ensureAdmin";
+
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
 const router = Router();
 
 router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
