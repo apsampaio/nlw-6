7322e363f11b6c9f6df53442bbf0385860862c72
apsampaio <andre03.sampaio@gmail.com>
2021-06-23 22:03:46 -0300

    add database relations to entity

--- valoriza/src/entities/Compliment.ts
+++ valoriza/src/entities/Compliment.ts
@@ -3,10 +3,14 @@ import {
   PrimaryColumn,
   Column,
   CreateDateColumn,
-  UpdateDateColumn,
+  JoinColumn,
+  ManyToOne,
 } from "typeorm";
 
 import { v4 as uuid } from "uuid";
+
+import { Tag } from "./Tag";
+import { User } from "./User";
 
 @Entity("compliments")
 class Compliment {
@@ -16,11 +20,23 @@ class Compliment {
   @Column()
   user_sender: string;
 
+  @JoinColumn({ name: "user_sender" })
+  @ManyToOne(() => User)
+  userSender: User;
+
   @Column()
   user_receiver: string;
 
+  @JoinColumn({ name: "user_receiver" })
+  @ManyToOne(() => User)
+  userReceiver: User;
+
   @Column()
   tag_id: string;
+
+  @JoinColumn({ name: "tag_id" })
+  @ManyToOne(() => Tag)
+  tag: Tag;
 
   @Column()
   message: string;
