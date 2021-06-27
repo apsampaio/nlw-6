3bfa6703598be59bc447ea84726f4c74e031123f
apsampaio <andre03.sampaio@gmail.com>
2021-06-26 22:30:27 -0300

    add project assets

---
+++ gameplay/src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { style } from "./styles";
+
+export function SignIn() {
+  return (
+    <View style={style.container}>
+      <Text>Texto</Text>
+    </View>
+  );
+}
