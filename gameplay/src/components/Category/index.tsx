7693c8371ebd0afd7f7ec41099e4a550abfbbbf0
apsampaio <andre03.sampaio@gmail.com>
2021-06-28 19:55:51 -0300

    minor fixes to category!

--- gameplay/src/components/Category/index.tsx
+++ gameplay/src/components/Category/index.tsx
@@ -10,25 +10,32 @@ import { theme } from "../../global/styles/theme";
 type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
+  hasCheckbox?: boolean;
   checked?: boolean;
 };
 
 export const Category: React.FC<Props> = ({
   title,
   icon: Icon,
+  hasCheckbox = false,
   checked = false,
   ...rest
 }) => {
-  const { secondary50, secondary70 } = theme.colors;
+  const { secondary40, secondary50, secondary70, secondary75 } = theme.colors;
 
   return (
     <RectButton style={style.container} {...rest}>
       <LinearGradient colors={[secondary50, secondary70]}>
-        <View style={[style.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? style.checked : style.check} />
+        <LinearGradient
+          style={[style.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[checked ? secondary75 : secondary50, secondary40]}
+        >
+          {hasCheckbox && (
+            <View style={checked ? style.checked : style.check} />
+          )}
           <Icon width={48} height={48} />
           <Text style={style.title}>{title}</Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
