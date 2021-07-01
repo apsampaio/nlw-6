c562e1a047ede8632f2b54fce560bcfe8a002f70
apsampaio <andre03.sampaio@gmail.com>
2021-06-30 21:28:18 -0300

    add banner

---
+++ gameplay/src/components/Member/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "react-native";
+
+import { style } from "./styles";
+import { theme } from "../../global/styles/theme";
+
+type Props = {
+  urlImage: string;
+};
+
+export const Member: React.FC<Props> = ({ urlImage }) => {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient style={style.container} colors={[secondary50, secondary70]}>
+      <Image source={{ uri: urlImage }} style={style.avatar} />
+    </LinearGradient>
+  );
+};
--- gameplay/src/screens/AppointmentDetails/index.tsx
+++ gameplay/src/screens/AppointmentDetails/index.tsx
@@ -3,24 +3,52 @@ import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { BorderlessButton } from "react-native-gesture-handler";
 
-import { View, FlatList } from "react-native";
+import { Text, ImageBackground, View, FlatList } from "react-native";
 import { style } from "./styles";
 
 import { Background } from "../../components/Background";
+import { ListHeader } from "../../components/ListHeader";
 import { Header } from "../../components/Header";
 import { theme } from "../../global/styles/theme";
 
+import BannerPNG from "../../assets/banner.png";
+
 export const AppointmentDetails: React.FC = () => {
+  const members = [
+    {
+      id: "1",
+      username: "Andre",
+      avatar_url: "https://github.com/apsampaio.png",
+      status: "online",
+    },
+    {
+      id: "2",
+      username: "Andre",
+      avatar_url: "https://github.com/apsampaio.png",
+      status: "offline",
+    },
+  ];
+
   return (
     <Background>
       <Header
         title="Detalhes"
         action={
           <BorderlessButton>
-            <Feather name="share" size={24} color={theme.colors.primary} />
+            <Feather name="share-2" size={24} color={theme.colors.primary} />
           </BorderlessButton>
         }
       />
+      <ImageBackground source={BannerPNG} style={style.banner}>
+        <View style={style.bannerContent}>
+          <Text style={style.title}>Lendários</Text>
+          <Text style={style.subtitle}>
+            É hoje que vamos ao challenger sem perder uma partida.
+          </Text>
+        </View>
+      </ImageBackground>
+      <ListHeader title="Jogadores" subtitle="Total 3" />
+      <FlatList data={members} />
     </Background>
   );
 };
