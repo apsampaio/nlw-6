9b3391f22cdcd57f22ca7d464e2d8971d4dcd97a
apsampaio <andre03.sampaio@gmail.com>
2021-07-09 13:03:31 -0300

    create first flutter app

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,38 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Meu Primeiro App Flutter")),
+      body: Center(
+          child: Text(
+        "Contador:\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
