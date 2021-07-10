5c09b14780f6695dbd8b3a8d6a4f3daa72d1dec2
apsampaio <andre03.sampaio@gmail.com>
2021-07-10 15:31:14 -0300

    add auth redirect and storage user

--- payflow/lib/modules/splash/splash_page.dart
+++ payflow/lib/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 
@@ -12,6 +13,10 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+
+    authController.currentUser(context);
+
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
